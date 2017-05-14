3fd878f30e08fbf72e1b090cfc19a10c26e71267
Viliami Tuanaki <Viliami@users.noreply.github.com>
2017-05-15 01:22:59 +1200

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,20 @@
-from graphics_pygame import *
+from Grapher import *
+from NN import *
 
-s = Screen(500,500,"kek")
+SCREEN_WIDTH, SCREEN_HEIGHT = (900,600)
+screen = Screen(SCREEN_WIDTH,SCREEN_HEIGHT,"Test")
+nn = NN(2,3,1)
 
-surf = Surface(300,300)
+#draw neural network structure
+structure = Structure(500, 300, nn)
+screen.addSurface(structure, 0, 0)
 
-s.addSurface(surf, 50,50)
+#add 2d neural grid vizualisation
+ngrid = NeuralGrid(200, 200, np.linspace(0,10,10), np.linspace(0,10,10), nn)
+screen.addSurface(ngrid, 50, 300)
 
-s.start()
+def onUpdate():
+    nn.train([[1,1],[2,2],[3,3],[3,4]],[[1],[1],[1],[0]])
+screen.onUpdate = onUpdate
+
+screen.start()
