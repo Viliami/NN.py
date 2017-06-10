5f461f3e696f4322c9d96cd26765cbb2478c31d8
Viliami Tuanaki <Viliami@users.noreply.github.com>
2017-06-10 23:19:49 +1200

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,11 +3,17 @@ from NN import *
 from Data import *
 
 data = Data()
-data.generateXORData(100, 0.2)
+# data.generateXORData(1000, 0.2)
+data.generateCircleData(500, 0.2)
+data.shuffle()
+
+testData = Data()
+# testData.generateXORData(50, 0.2)
+testData.generateCircleData(50, 0.2)
 
 nn = NN(2,4,3,1)
-nn.activationFunction = "tanh"
-nn.learningRate = 0.0009
+nn.activationFunction = "sigmoid"
+nn.learningRate = 0.03
 
 SCREEN_WIDTH, SCREEN_HEIGHT = (900,600)
 screen = Screen(SCREEN_WIDTH,SCREEN_HEIGHT,"Test")
@@ -23,19 +29,23 @@ screen.addSurface(ngrid, 50, 300)
 
 #add time series visualisaiton
 costGraph = TimeSeries(200,200, np.linspace(0,10,10))
+costGraph.setBackgroundColor((240,240,240))
 screen.addSurface(costGraph, 500,0)
 
 epoch = 0
+nn.learningRate = 1
+decay_steps = 20
 def onUpdate():
-    global epoch
+    global epoch,decay_steps
+    # nn.learningRate -= 0.007 * nn.learningRate
+    nn.learningRate *= 0.96**(epoch//decay_steps)
+    if(epoch//decay_steps):
+        decay_steps += 20
     epoch += 1
-    print(epoch)
-    # nn.train([[1,1],[2,2],[3,3],[3,4]],[[1],[1],[1],[0]])
-    # nn.train(data)
-    # costGraph.plot(nn.evaluate(np.array([[1,1],[2,2],[3,3],[3,4]]),np.array([[1],[1],[1],[0]]))*35)
-    nn.train(data)
-    costGraph.plot(nn.evaluate(data.inputs,data.outputs)*30)
-    # print(nn.evaluate(data.inputs,data.outputs))
+    accuracy = nn.evaluate(testData.inputs,testData.outputs)
+    print(epoch,nn.learningRate,accuracy[0])
+    nn.minibatch(data,10)
+    costGraph.plot(accuracy*30)
 screen.onUpdate = onUpdate
 
 screen.start()
