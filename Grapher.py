b4273b6148a5ea3e435ca88a7c9d8e5c8164976f
Viliami Tuanaki <Viliami@users.noreply.github.com>
2017-10-10 19:14:08 +1300

    created Grid visualization class

--- Grapher.py
+++ Grapher.py
@@ -1,8 +1,12 @@
 # from graphics_qt import *
 from graphics_pygame import *
-#from NN import *
+from colour import Color
 import numpy as np
 import math
+
+#TODO: create text labels for axis
+#TODO: implement color gradients
+#TODO: draw arrows
 
 class Graph2D(Surface):
     def __init__(self, width, height, xAxis, yAxis):
@@ -100,31 +104,120 @@ class NeuralGrid(Graph2D): #only possible if there are at least 2 input neurons 
                 c = min(255, max(0, self.data.outputs[i,0] * 255))
                 self.renderPoint(((x,y),(0,c,c),3))
 
-class HeatMap(Graph2D):
-    def __init__(self, width, height, xAxis, yAxis, function = None,data=None):
-        super().__init__(width, height, xAxis, yAxis)
+class Grid(Graph2D):
+    def __init__(self, width, height, xAxis, yAxis, data=None):
+        super().__init__(width,height,xAxis,yAxis)
         self.data = data
         coord = []
         for x in self.xAxis:
             for y in self.yAxis:
                 coord.append(np.array([x,y]))
         self.coordinates = np.array(coord)
+        self.steps = 129
+        self.colors = list(Color("red").range_to(Color("blue"),self.steps))
+        self.offset = 70
+        #self.path = [(10,10),(25,10),(100,50),(150,100)]
+        self.path = []
+        self.points = [(150,110)]
+
+    def toPixel(self, cX, cY): #coordinates to pixels
+        cX -= self.xAxis[0]
+        cY -= self.yAxis[0]
+        xRatio = self.width/len(self.xAxis)
+        yRatio = self.height/len(self.yAxis)
+        return (int(cX*xRatio), int(cY*yRatio))
+
+    def axisToPixel(self, cX, cY):
+        offset = self.offset
+        cX -= self.xAxis[0]
+        cY -= self.yAxis[0]
+        w,h = self.width-offset, self.height-offset
+        xRatio = w/len(self.xAxis)
+        yRatio = h/len(self.yAxis)
+        return (int(cX*xRatio)+offset, int(cY*yRatio)+offset)
 
     def render(self):
+        width = math.ceil(self.width/len(self.xAxis))
         self.fill(self.backgroundColor)
-        a = function(self.coordinates).T
+        a = self.data
+        amax = np.amax(a)
         counter = 0
-        for x in self.xAxis:
-            for y in self.yAxis:
-                c = min(255, max(0,a[counter]*255))
-                self.filledSquare(self.toPixel(x,y),11,(0,c,c))
+        for y in self.yAxis:
+            for x in self.xAxis:
+                c = a[int(x)][int(y)]*(((self.steps-1)//2)/amax)
+                col = self.colors[int(c)].rgb
+                #self.filledSquare(self.toPixel(x+offset,y+offset),width,(col[0]*255,col[1]*255,col[2]*255),(col[0]*255,col[1]*255,col[2]*255))
+                self.filledSquare(self.axisToPixel(x,y),width,(col[0]*255,col[1]*255,col[2]*255),(col[0]*255,col[1]*255,col[2]*255))
                 counter+=1
 
-        if(self.data):
-            for i in range(len(self.data.inputs)):
-                x,y = self.data.inputs[i]
-                c = min(255, max(0, self.data.outputs[i,0] * 255))
-                self.renderPoint(((x,y),(0,c,c),3))
+        #draw axis labels
+        for y in self.yAxis:
+            if(y%25 == 0):
+                #self.text(self.toPixel(3,y+offset-5),str(int(y)),color=(255,0,0))
+                self.text(self.axisToPixel(-20,y-5),str(int(y)))
+                #self.line(self.toPixel(offset*(3/4),y),self.toPixel(offset,y),color=(255,0,0))
+                self.line(self.axisToPixel(-5,y),self.axisToPixel(0,y))
+
+        for x in self.xAxis:
+            if(x%25 == 0):
+                #self.text(self.toPixel(x+offset-5,5),str(int(x)))
+                self.text(self.axisToPixel(x-5,-15),str(int(x)))
+                #self.line(self.toPixel(x,offset*(3/4)),self.toPixel(x,offset),width=3)
+                self.line(self.axisToPixel(x,-5),self.axisToPixel(x,0))
+
+        #draw border
+        '''self.line((0,1),(self.width,1),width=3)
+        self.line((self.width-1,0),(self.width-1,self.height),width=3)
+        self.line((1,0),(1,self.height),width=3)
+        self.line((0,self.height-1),(self.width,self.height-1),width=3)'''
+
+        #self.drawScatter(self.points)
+        self.drawPath(self.path)
+
+    def drawPath(self, path):
+        prevPoint = path[0]
+        circleColor = (100,255,0)
+        lineColor = (0,255,0)
+        lineColor = (0,0,0)
+        color = (0,255,0)
+        lastPoint = path[-1]
+        for point in path:
+            if(point is lastPoint):
+                break
+            x,y = point
+            x2,y2 = prevPoint
+            self.line(self.axisToPixel(x,y),self.axisToPixel(x2,y2),lineColor,width=4)
+            self.filledCircle(self.axisToPixel(x,y), 5, circleColor)
+            prevPoint = point
+
+        #draw arrowhead for last point
+        lastPoint = path[-1]
+        lx,ly = lastPoint
+        secondLastPoint = path[-2]
+        l2x,l2y = secondLastPoint
+        l = np.array([lx,ly])
+
+        h = 7
+        w = 0.8
+
+        dx = lx-l2x
+        dy = ly-l2y
+        gradient = dx/dy
+        length = math.sqrt(dx**2 + dy**2)
+        dir = np.array([dx/length,dy/length])
+        qx = dy
+        qy = -dx
+        q = np.array([qx,qy])
+        left = self.axisToPixel(*(l-(h*dir)+(w*q/2.0)))
+        right = self.axisToPixel(*(l-(h*dir)-(w*q/2.0)))
+        l = self.axisToPixel(*l)
+        self.polygon([left,l,right])
+        self.polygon([left,l,right],color,1)
+
+    def drawScatter(self, points):
+        for point in points:
+            x,y = point
+            self.filledCircle(self.axisToPixel(x,y),3)
 
 class Structure(Surface):
     def __init__(self, width, height, nn):
@@ -158,7 +251,7 @@ class Structure(Surface):
                         wcolor = (200,0,0)
                         weight = -weight
                     screen.line((x,y), (x-x_delta,y_pad+(temp_y_delta//2)+int(temp_y_delta*k)), wcolor, weight)
-                y+=y_delta
+                y += y_delta
             x += x_delta
 
 class ImgArray(Surface):
